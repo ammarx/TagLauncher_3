c67fb5aeb6d1926e0e6ce606e2ef6e6f36a418ad
Ammar Ahmad <ammar.ahmad1993@gmail.com>
2016-08-30 13:26:34 +0500

    implement sync profile

--- src/taglauncher_3/Controller.java
+++ src/taglauncher_3/Controller.java
@@ -5,9 +5,15 @@
  */
 package taglauncher_3;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.Hashtable;
 import java.util.Optional;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -62,26 +68,63 @@ public class Controller implements Initializable {
         //load data on different thread.
         ExecutorService executor = Executors.newCachedThreadPool();
         executor.submit(() -> {
+            installversionList.setDisable(true);
+
             API.downloadVersionManifest();
+
+            for (Object ob : API.getInstallableVersionsList()) {
+                String[] prsntAry = ob.toString().split(" % ");
+                installversionList.getItems().addAll(prsntAry[0]);
+                VersionHashTable.put(prsntAry[0], prsntAry[1]);
+
+            }
+            //step 2 read files from local system
+            if (!API.getInstalledVersionsList().isEmpty()) {
+
+                for (Object ob_ : API.getInstalledVersionsList()) {
+                    if (!VersionHashTable.containsKey((String) ob_)) {
+                        installversionList.getItems().addAll(ob_);
+                        VersionHashTable.put((String) ob_, "Unknown");
+                    }
+                }
+
+            }
+            installversionList.setDisable(false);
+
             return null;
         });
         executor.shutdown();
-        installversionList.setDisable(true);
-        for (Object ob : API.getInstallableVersionsList()) {
-            String[] prsntAry = ob.toString().split(" % ");
-            installversionList.getItems().addAll(prsntAry[0]);
-            VersionHashTable.put(prsntAry[0], prsntAry[1]);
 
-        }
-        //step 2 read files from local system
-        for (Object ob_ : API.getInstalledVersionsList()) {
-            if (!VersionHashTable.containsKey((String) ob_)) {
-                installversionList.getItems().addAll(ob_);
-                VersionHashTable.put((String) ob_, "Unknown");
+        //load my settings
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+
+            input = new FileInputStream("config.properties");
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            username.setText(prop.getProperty("username"));
+            for (Object ob : API.getInstalledVersionsList()) {
+                if (ob.equals(prop.getProperty("version"))) {
+                    version.setValue(prop.getProperty("version"));
+                }
+            }
+
+        } catch (IOException ex) {
+            System.out.println("File not found" + ex);
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    System.out.println("File not found" + e);
+                }
             }
         }
-
-        installversionList.setDisable(false);
 
     }
 
@@ -89,10 +132,13 @@ public class Controller implements Initializable {
     private void lunachMineCraft(ActionEvent event) {
         //txt.getText()
         //(String) cmbox.getValue()
+        launch.setDisable(true);
         tagapi_3.API_Interface API = new tagapi_3.API_Interface();
 
         ExecutorService executor = Executors.newCachedThreadPool();
         executor.submit(() -> {
+            API.downloadProfile((String) version.getValue());
+            API.syncVersions();
             API.downloadMinecraft((String) version.getValue(), false); //force download flag
             API.runMinecraft(username.getText(), (String) version.getValue());
             return null;
@@ -109,10 +155,37 @@ public class Controller implements Initializable {
                     Thread.sleep(10);
 
                     if (API.getLog().equals("[rl] Minecraft Initialized!")) {
+                        //write settings to system
+                        Properties prop = new Properties();
+                        OutputStream output = null;
+
+                        try {
+
+                            output = new FileOutputStream("config.properties");
+
+                            // set the properties value
+                            prop.setProperty("username", username.getText());
+                            prop.setProperty("version", (String) version.getValue());
+
+                            // save properties to project root folder
+                            prop.store(output, null);
+
+                        } catch (IOException io) {
+                            io.printStackTrace();
+                        } finally {
+                            if (output != null) {
+                                try {
+                                    output.close();
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                        }
                         API.dumpLogs();
                         System.exit(0);
                         return;
-                    } else if (API.getLog().equals("[rl] Minecraft Corruption found!")) {
+                    } else if (API.getLog().equals("[el] Minecraft Corruption found!")) {
                         Platform.runLater(() -> {
                             status.setText(API.getLog());
                             Alert alert = new Alert(AlertType.ERROR);
@@ -121,6 +194,7 @@ public class Controller implements Initializable {
                             alert.setContentText("The game failed to initialize as data corruption \nwas found! Press re-Download game with \n*Force Download* checked");
                             alert.showAndWait();
                             API.dumpLogs();
+                            launch.setDisable(false);
                             //refresh versions list
 
                             //System.out.println(API.getRunLogs());
@@ -152,6 +226,9 @@ public class Controller implements Initializable {
     @FXML
     private void installGameBtn(ActionEvent event) {
         //step 1 check if forceinstall is enabled or not
+        launch.setDisable(true);
+        installGame.setDisable(true);
+
         if (forceInst.isSelected()) {
             System.out.println("Selected!");
         } else {
@@ -189,6 +266,9 @@ public class Controller implements Initializable {
                             alert.setTitle("Download Complete!");
                             alert.setHeaderText(null);
                             alert.setContentText("Version: " + (String) installversionList.getValue() + " has been installed!");
+
+                            launch.setDisable(false);
+                            installGame.setDisable(false);
 
                             alert.showAndWait();
                             API.dumpLogs();
